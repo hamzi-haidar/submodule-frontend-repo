7d168a711f6e348ed32ca89d8cf6ca5f201d259e
Hamzi Haidar <hamzihaider@gmail.com>
2024-07-07 12:03:56 +0300

    refactored the code

--- clients.py
+++ clients.py
@@ -2,10 +2,14 @@ import yaml
 import os
 
 def read_config():
-    config_path = os.path.join(os.path.dirname(__file__), '..', 'fullstack-repo', 'config.yaml')
+    config_path = os.path.join(os.path.dirname(__file__), '../config.yaml')
+ 
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
 config = read_config()
-print(config['ServerIPAddress'])
+
+print( config)
+
+
